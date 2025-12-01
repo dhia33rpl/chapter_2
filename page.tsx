faae368e17b95d61aba634add0e27a5fa9188a72
Raihanah_33rpl <raihanahnafidhiasonyata@gmail.com>
2025-12-01 10:21:47 +0700

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,57 +1,24 @@
-"use client";
+import { AlertDanger, AlertInfo, AlertSuccess, AlertWarning } from "@/components/alert";
 
-import {
-  ButtonSuccess,
-  ButtonWarning,
-  ButtonDanger,
-  OutLineSuccess,
-  OutLineWarning,
-  OutLineDanger,
-} from "../../components/buttom";
 
-const TestButton = () => {
-  return (
-    <div className="m-10 p-10">
-      <ButtonSuccess
-        type="button"
-        onClick={() => alert("Success button clicked!")}
-      >
-        Success Button
-      </ButtonSuccess>
-      <ButtonWarning
-        type="button"
-        className="ml-2"
-        onClick={() => alert("Warning button clicked!")}
-      >
-        Warning Button
-      </ButtonWarning>
-      <ButtonDanger
-        type="button"
-        className="ml-2"
-        onClick={() => alert("Danger button clicked!")}
-      >
-        Danger Button
-      </ButtonDanger>
-      <OutLineSuccess
-        type="button"
-        onClick={() => alert("Success button clicked!")}
-      >
-        Success Outline Button
-      </OutLineSuccess>
-      <OutLineWarning
-        type="button"
-        onClick={() => alert("Warning button clicked!")}
-      >
-        Warning Outline Button
-      </OutLineWarning>
-      <OutLineDanger
-        type="button"
-        onClick={() => alert("Warning button clicked!")}
-      >
-        Danger Outline Button
-      </OutLineDanger>
-    </div>
-  );
-};
+const TestAlert = () => {
+   return(
+       <div className="m-10">
+           <AlertInfo title="Alert Info">
+               Ini adalah Alert dengan tipe info
+           </AlertInfo>
+           <AlertSuccess title="Alert Success">
+               Ini adalah alert dengan tipe Success
+           </AlertSuccess>
+           <AlertWarning title="Alert Warning">
+               Ini adalah alert tipe warning
+           </AlertWarning>
+           <AlertDanger title="Alert Danger">
+               Ini adalah alert tipe dangger
+           </AlertDanger>
+       </div>
+   )
+}
 
-export default TestButton;
+
+export default TestAlert;
